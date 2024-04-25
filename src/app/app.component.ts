461cce6bd7fa2937d06ff3f7f4098f06287bc2db
jeyofdev <jgregoire.dev@gmail.com>
2024-04-25 19:26:59 +0200

    feat : get pokemons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from './services/pokemon.service';
+import { PokemomResponse, Pokemon } from './models/pokemon.type';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'service-observable';
+export class AppComponent implements OnInit {
+    pokemons!: Pokemon[];
+    pokemonsLength!: number;
+
+    constructor(private pokemonService: PokemonService) {}
+
+    ngOnInit(): void {
+        this.pokemonService
+            .getPokemons(3)
+            .subscribe((pokemonResponse: PokemomResponse) => {
+                this.pokemons = pokemonResponse.results;
+                this.pokemonsLength = this.pokemons.length;
+            });
+    }
 }
---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { PokemomResponse, Pokemon } from '../models/pokemon.type';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokemonService {
+  baseUrl: string = 'https://pokeapi.co/api/v2/pokemon/';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getPokemons(limit: number): Observable<PokemomResponse> {
+    return this.httpClient
+      .get<PokemomResponse>(`${this.baseUrl}?limit=${limit}`)
+      .pipe(map((pokemon) => pokemon));
+  }
+}
